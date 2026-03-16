f21466c05ec776eec4d0b0e5e3e0bb784a2f0ed0
Cary Lee <clee@mdclarity.com>
2026-03-16 12:02:17 -0700

    Add terraform flake

---
+++ dev-envs/terraform/flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs-terraform.url = "github:stackbuilders/nixpkgs-terraform";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  nixConfig = {
+    extra-substituters = "https://nixpkgs-terraform.cachix.org";
+    extra-trusted-public-keys = "nixpkgs-terraform.cachix.org-1:8Sit092rIdAVENA3ZVeH9hzSiqI/jng6JiCrQ1Dmusw=";
+  };
+
+  outputs = { self, nixpkgs-terraform, nixpkgs, systems }:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+            terraform = nixpkgs-terraform.packages.${system}."1.9.4";
+          in
+          {
+            default = pkgs.mkShell {
+              buildInputs = [ terraform ];
+            };
+          });
+    };
+}

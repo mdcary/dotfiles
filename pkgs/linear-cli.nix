fadc13aff76aeed070e4f481917eef3b08cd8d0d
Cary Lee <clee@mdclarity.com>
2026-03-16 17:51:50 -0700

    Change build setup

--- pkgs/linear-cli.nix
+++ pkgs/linear-cli.nix
@@ -2,36 +2,28 @@
 
 pkgs.stdenv.mkDerivation rec {
   pname = "linear-cli";
-  version = "latest"; # You can pin a specific version here if you prefer
+  version = "1.11.1"; # Current version as of early 2026
 
-  src = pkgs.fetchFromGitHub {
-    owner = "schpet";
-    repo = "linear-cli";
-    rev = "v1.11.1"; # Or a specific tag like "v0.1.0"
-    hash = "sha256-os/p8P1ZFqdMFuqci0XtbDpJQP31PDfCtYSX9xFv8D4=";
+  src = pkgs.fetchurl {
+    # Note: Using the x86_64-linux binary. 
+    # If you are on ARM (Apple Silicon via WSL/VM), use 'aarch64-unknown-linux-gnu'
+    url = "https://github.com/schpet/linear-cli/releases/download/v${version}/linear-x86_64-unknown-linux-gnu.tar.xz";
+    sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; 
   };
 
-  nativeBuildInputs = [ pkgs.deno ];
-
-  buildPhase = ''
-    # Deno needs a writable HOME for caching during the build
-    export HOME=$TMPDIR
-  '';
+  # This source is a tarball, so Nix will unpack it automatically.
+  # We just need to move the binary to the right place.
+  setSourceRoot = "sourceRoot = \".\";";
 
   installPhase = ''
     mkdir -p $out/bin
-    # We use deno install to create the executable wrapper
-    deno install \
-      --root $out \
-      --allow-all \
-      --name linear \
-      ./main.ts
+    cp linear $out/bin/
+    chmod +x $out/bin/linear
   '';
 
   meta = with pkgs.lib; {
     description = "A CLI to list, start and create issues in Linear";
     homepage = "https://github.com/schpet/linear-cli";
     license = licenses.mit;
-    maintainers = [ ];
   };
 }

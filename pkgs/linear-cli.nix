84d2801cbbda68d0060a33821d1277e24db5fc2f
Cary Lee <clee@mdclarity.com>
2026-03-16 18:04:43 -0700

    Try deno2nix version

--- pkgs/linear-cli.nix
+++ pkgs/linear-cli.nix
@@ -1,34 +1,12 @@
 { pkgs, ... }:
 
-pkgs.stdenv.mkDerivation rec {
-  pname = "linear-cli";
-  version = "1.11.1";
-
-  src = pkgs.fetchurl {
-    url = "https://github.com/schpet/linear-cli/releases/download/v${version}/linear-x86_64-unknown-linux-gnu.tar.xz";
-    # This is the EXACT checksum from the official installer script!
-    sha256 = "4bbcf0c4e617c1898afb3732ba1baf556f098573c8e6181a5a2c84cfb4f48082";
-  };
-
-  # The installer script confirms it uses --strip-components 1
-  # This means Nix should look for the binary in the root of the unpacked source.
-  sourceRoot = ".";
-
-  # We need to add the unpacker for .tar.xz
-  nativeBuildInputs = [ pkgs.xz ];
-
-  installPhase = ''
-    mkdir -p $out/bin
-    # The installer shows the binary is just named 'linear'
-    # We find it and move it to our bin directory
-    find . -type f -name "linear" -exec cp {} $out/bin/ \;
-    chmod +x $out/bin/linear
-  '';
-
-  meta = with pkgs.lib; {
-    description = "Linear CLI - list, start, and create issues";
-    homepage = "https://github.com/schpet/linear-cli";
-    license = licenses.mit;
-    platforms = [ "x86_64-linux" ];
-  };
-}
+# We create a wrapper that mimics the installer's result 
+# but uses the source code to avoid the binary corruption.
+pkgs.writeShellScriptBin "linear" ''
+  # We use the Deno from your Nix store
+  # We point it directly at the main entry point of the tool
+  exec ${pkgs.deno}/bin/deno run \
+    --allow-all \
+    --no-check \
+    https://raw.githubusercontent.com/schpet/linear-cli/main/main.ts "$@"
+''
